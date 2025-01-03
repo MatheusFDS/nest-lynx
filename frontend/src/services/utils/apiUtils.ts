14099b6b662d240c9532596b21036ceb3615e760
MatheusFds <matheussilva_1994@hotmail.com>
2025-01-03 11:52:51 -0300

    remove vercel

--- frontend/src/services/utils/apiUtils.ts
+++ frontend/src/services/utils/apiUtils.ts
@@ -1,1 +1,6 @@
-export const getApiUrl = () => "/api";
+const API_BACK = process.env.NEXT_PUBLIC_API_URL as string;
+
+export const getApiUrl = () => {
+  // Sempre retorna a URL do backend
+  return API_BACK;
+};
