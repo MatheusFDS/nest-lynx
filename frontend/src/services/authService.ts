fff330c1577cda255b88f662db93da1cb7431962
MatheusFds <matheussilva_1994@hotmail.com>
2025-01-03 12:19:13 -0300

    att vercel 3

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,5 +1,6 @@
 // src/services/authService.ts
 import { getApiUrl } from './utils/apiUtils';
+
 export const getStoredToken = () => {
     return localStorage.getItem('token');
   };
@@ -13,7 +14,7 @@ export const getStoredToken = () => {
   };
   
   export const refreshAccessToken = async (refreshToken: string) => {
-    const response = await fetch(getApiUrl + '/auth/refresh-token', {
+    const response = await fetch(getApiUrl() + '/auth/refresh-token', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- frontend/src/services/utils/apiUtils.ts
+++ frontend/src/services/utils/apiUtils.ts
@@ -1,6 +1,1 @@
-const API_BACK = process.env.NEXT_PUBLIC_API_URL as string;
-
-export const getApiUrl = () => {
-  // Sempre retorna a URL do backend
-  return API_BACK;
-};
+export const getApiUrl = () => "/api";
