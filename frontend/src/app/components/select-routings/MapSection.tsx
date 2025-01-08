df88d386dcbb48a8c1ea7f0c986ed4ccd7299c27
MatheusFds <matheussilva_1994@hotmail.com>
2025-01-08 13:35:50 -0300

    att modal do mapa 6

--- frontend/src/app/components/select-routings/MapSection.tsx
+++ frontend/src/app/components/select-routings/MapSection.tsx
@@ -31,11 +31,24 @@ const MapSection: React.FC<MapSectionProps> = ({
           backgroundColor: isDarkMode ? '#121212' : '#fff',
           color: isDarkMode ? '#fff' : '#000',
           overflow: 'hidden',
-          padding: 5,
-          margin: 0,
+          padding: '20px', // Aumentei o padding para melhor visualização
+          borderRadius: '8px', // Adicionei bordas arredondadas
+          // Estilos para centralizar o modal
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          // Opcional: definir largura e altura
+          maxWidth: '90%',
+          maxHeight: '90%',
         },
         overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.75)'
+          backgroundColor: 'rgba(0, 0, 0, 0.75)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
         },
       }}
     >
